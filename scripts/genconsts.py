9fb3bec4ec410935bb81cc4e6918d11bca05726b
lukastyrychtr <lukastyrychtr@d1988a6a-2406-46f6-9be7-c0275a28d9d7>
2010-12-31 21:29:10 +0000

    Now, our new method is able to generate enums just fine, structures are relatively good.
    git-svn-id: https://pyfmodex.svn.sourceforge.net/svnroot/pyfmodex@22 d1988a6a-2406-46f6-9be7-c0275a28d9d7

--- scripts/genconsts.py
+++ scripts/genconsts.py
@@ -1,104 +1,99 @@
-#This script analyzes the fmod header file and generates structures.py and constants.py.
-#We're using pygccxml for our hard parsing job.
+#This script analyzes the fmod header file and generates structures.py, enums.py and callbackprototypes.py.
+#We're going to parse the xml output from gccxml's work.
 #We are using following mapping from c types to ctypes equivalents
 import os
-from pygccxml import parser, declarations
+import xml.etree.ElementTree as et
 types_map = {"int": "c_int", "float": "c_float", "char*": "c_char_p", "void*": "c_void_p", "unsigned short": "c_ushort", "unsigned int": "c_uint", "enum":"c_int", "short unsigned int":"c_ushort", "unsigned char":"c_ubyte","char":"c_char","void":"c_void"}
+#Which types are function types?
+function_types = []
 initial_lines = "from ctypes import *\n"
-constants = initial_lines
+enums = initial_lines
 structures = initial_lines + "\nfrom callbackprototypes import *\n"
 callbackprototypes = initial_lines + "\nimport os\nfunc = WINFUNCTYPE if os.name == \"nt\" else CFUNCTYPE"
-gc_path = r"c:\gccxml\bin"
-fmod_h_path = r"c:\gccxml\bin\fmod.h"
-cfg = parser.config_t(gccxml_path=gc_path)
-ns_list = parser.parse([fmod_h_path], cfg)
-ns = ns_list[0]
 
-def get_type(type):
-    print "Request for type %s"%type
-    if isinstance(type, declarations.fundamental_t): return types_map[str(type)]
-    if isinstance(type, declarations.enumeration_t): return "enum"
-    if isinstance(type, declarations.array_t):
-        basetype = get_type(type.base)
-        return "%s * %i"%(basetype,type.size)
-    if isinstance(type, declarations.pointer_t):
-        if isinstance(type.base, declarations.const_t): return types_map["%s*"%str(type.base.base)]
+def create_enum_code(enum_elem):
+    global enums, types_map
+    name = enum_elem.attrib["name"]
+    #We know about new type, so we're going to add it to our mapping
+    types_map[name] = "c_int"
+    enums += "#%s enumeration.\n"%name
+    for enum_val in enum_elem.getchildren():
+        enums += "%s = %s\n"%(enum_val.attrib["name"], enum_val.attrib["init"])
+    enums += "\n"
 
-def gen_callback(name, func):
+def create_function_type_code(func_name, func_elem):
     global callbackprototypes
-    print "generating callback %s"%name
-    callbackprototypes += "\n%s = func("%name
-    if hasattr(func.return_type, "declaration"):
-        rdec = func.return_type.declaration
-    else:
-        rdec = func.return_type
-    rettype = get_type(rdec)
-    if not rettype: introduce_type(rdec)
-    callbackprototypes += "%s,"%get_type(func.return_type)
-    for argtype in func.arguments_types:
-        at = get_type(argtype)
-        if not at: introduce_type(argtype)
-        callbackprototypes += "%s,"%get_type(argtype)
-    callbackprototypes = callbackprototypes[:-1]
-    callbackprototypes += ")\n"
-   
-def introduce_type(t):
-    print "introducing type %s of class %s"%(t,str(type(t)))
-    if hasattr(t, "type"):
-        tvar = t.type
-    else:
-        tvar = t
-    guess = get_type(tvar)
-    if guess:
-        types_map[t.name] = guess    
-    else:
-        if isinstance(tvar, declarations.pointer_t):
-            p = tvar
-            if isinstance(p.base, declarations.free_function_type_t):
-                gen_callback(t.name, p.base)
-                types_map[t.name] = t.name
+    print func_name + " - generating."
+    callbackprototypes += "%s = func(%s,"%(func_name,get_type(elems_by_id[func_elem.attrib["returns"]]))
+    for arg in func_elem.getchildren():
+        callbackprototypes += " %s,"%get_type(elems_by_id[arg.attrib["type"]], func_name)
+    callbackprototypes += callbackprototypes[:-1] + ")\n"
 
-def gen_enum(enum):
-    global constants
-    """Appends code for one enumeration type."""
-    constants += "\n#Definition for %s enumeration.\n"%enum.name
-    for name, value in enum.values:
-        constants += "%s = %s\n"%(name, value)
+def create_struct_code(struct_elem):
+    global structures
+    if struct_elem.attrib.has_key("incomplete"): return #Skip all incomplete structures, we have dealed with them earlier.
+    structures += "class %s(Structure):\n"%struct_elem.attrib["name"]
+    structures += " " * 4 + "_fields_ = [\n"
+    for mid in struct_elem.attrib["members"].split():
+        m = elems_by_id[mid]
+        if m.tag == "Field": #Structure field.
+            structures += " " * 8 + "(\"%s\", %s)\n"%(m.attrib["name"], get_type(elems_by_id[m.attrib["type"]]))
+    structures += " " * 4 + "]\n"
 
-def gen_struct(s):
-    global structures    
-    structures += "\nclass %s(Structure):\n"%s.name
-    structures += "    _fields_ = [\n"
-    fields = [f for f in s.public_members if type(f) == declarations.variable_t]
-    currfield = 1
-    for f in fields:
-        if currfield != len(fields):
-            tpl = "        (\"%s\", %s),\n"
-        else:
-            tpl = "        (\"%s\", %s)\n"
-        structures += tpl%(f.name, get_type(f.type))
-        currfield += 1
-    structures += "    ]\n"
-    fnames = [f.name for f in fields]
-    if "cbsize" in fnames:
-        structures += "\n    def __init__(self, *args, **kwargs):\n        super(%s, self).__init__(self, *args, **kwargs)\n        self.cbsize = sizeof(self)\n"%s.name
-
-#Introduce custom fmod ex types for next time reference
-for t in ns.typedefs():
-    introduce_type(t)    
-#Enumerations
-for e in ns.enums():
-    gen_enum(e)
-#structures
-for c in ns.classes():
-    gen_struct(c)
-cur_dir = os.path.dirname(__file__)
-sf = open(os.path.join(cur_dir, "structures.py"), "w")
-sf.write(structures)
-sf.close()
-cf = open(os.path.join(cur_dir, "constants.py"), "w")
-cf.write(constants)
-cf.close()
-cf = open(os.path.join(cur_dir, "callbackprototypes.py"), "w")
-cf.write(callbackprototypes)
-cf.close()
+def get_type(type_elem, type_name=""):
+    global types_map, function_types
+    #If we don't hawe name of the type, we're going to handle new type.
+    if not type_name:
+        print "Name not known from param, tag %s"%type_elem.tag
+        type_name = type_elem.attrib.get("name", "unknown")
+    #We could have seen this type in some other call, so we'll try it.
+    if types_map.has_key(type_name):
+        return types_map[type_name]
+    #Then, we should check if this isn't only a reference for somethink else.
+    if type_elem.attrib.has_key("type"):
+        t = get_type(elems_by_id[type_elem.attrib["type"]], type_name)
+        #Add it for later time.
+        types_map[type_name] = t
+        return t
+    #If this is a fundamental type, we already know about the right definition.
+    if type_elem.tag == "FundamentalType":
+        return types_map[type_elem.attrib["name"]]
+    #Or, it might be a function type definition?
+    elif type_elem.tag == "FunctionType":
+        function_types.append(type_name)
+        create_function_type_code(type_name, type_elem)
+        return type_name #We can return same name, because we're really referencing function prototype object later.
+doc = et.parse("fmodout.xml")
+root = doc.getroot()
+#generate enumerations
+print "Generating enumerations..."
+for enum in root.findall("Enumeration"):
+    create_enum_code(enum)
+#Now, before we continue, we'll introduce some types as c_ints. These types are incomplete structures for c compilers.
+print "Adding incomplete structures as c_ints..."
+for st in root.findall("Struct"):
+    if st.attrib.has_key("incomplete"):
+       types_map[st.attrib["name"]] = "c_int"
+#Now, we'll introduce mapping from ids to etree elements.
+elems_by_id = {}
+print "Generating elems_by_id dict..."
+for el in root.getchildren():
+    elems_by_id[el.attrib["id"]] = el
+#Now, add all remaining unknown types.
+print "Adding remaining type declarations and generating callback prototypes."
+for td in root.findall("Typedef"):
+    t = get_type(td)
+#After this, structures are waiting for us.
+print "Generating structures..."
+for st in root.findall("Struct"):
+    create_struct_code(st)
+#Final part, write the results.
+f = open("enums.py", "w")
+f.write(enums)
+f.close()
+f = open("structures.py", "w")
+f.write(structures)
+f.close()
+f = open("callbackprototypes.py", "w")
+f.write(callbackprototypes)
+f.close()
