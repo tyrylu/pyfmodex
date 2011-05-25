1bbc442e2ae60501cc6ad65b3b48e14e30e6c051
lukastyrychtr <lukastyrychtr@d1988a6a-2406-46f6-9be7-c0275a28d9d7>
2011-05-25 17:02:26 +0000

    Added sound group api, hopefully, of course...
    git-svn-id: https://pyfmodex.svn.sourceforge.net/svnroot/pyfmodex@30 d1988a6a-2406-46f6-9be7-c0275a28d9d7

--- pyfmodex/sound_group.py
+++ pyfmodex/sound_group.py
@@ -1,4 +1,75 @@
+import system, sound
 from fmodobject import *
 
 class SoundGroup(FmodObject):
-    pass
+
+@property    
+def max_audible(self):
+        val = c_int()
+    ckresult(_dll.FMOD_SoundGroup_GetMaxAudible(self._ptr, byref(val)))
+    return val.value
+    @max_audible.setter
+    def max_audible(self, val):
+        ckresult(_dll.FMOD_SoundGroup_SetMaxAudible(self._ptr, val))
+
+    @property
+    def max_audible_behavior(self):
+        behavior = c_int()
+        ckresult(_dll.FMOD_SoundGroup_GetMaxAudibleBehavior(self._ptr, byref(behavior)))
+        return behavior.value
+    @max_audible_behavior.setter
+    def max_audible_behavior(self, behavior):
+        ckresult(_dll.FMOD_SoundGroup_SetMaxAudibleBehavior(self._ptr, behavior))
+
+    @property
+    def mute_fade_speed(self):
+        speed = c_float()
+        ckresult(_dll.FMOD_SoundGroup_GetMuteFadeSpeed(self._ptr, byref(speed)))
+        return speed.value
+    @mute_fade_speed.setter
+    def mute_fade_speed(self, speed):
+        ckresult(_dll.FMOD_SoundGroup_SetMuteFadeSpeed(self._ptr, speed))
+
+    @property
+    def name(self):
+        buf = create_string_buffer(512)
+        ckresult(_dll.FMOD_SoundGroup_GetName(self._ptr, buf, 512))
+        return buf.value
+
+    @property
+    def num_playing(self):
+        num = c_int()
+        ckresult(_dll.FMOD_SoundGroup_GetNumPlaying(self._ptr, byref(num)))
+        return num.value
+
+    @property
+    def num_sounds(self):
+        num = c_int()
+        ckresult(_dll.FMOD_SoundGroup_GetNumSounds(self._ptr, byref(num)))
+        return num.value
+
+    def get_sound(self, idx):
+        sndptr = c_int()
+        ckresult(_dll.FMOD_SoundGroup_GetSound(self._ptr, idx, byref(sndptr)))
+        return sound.Sound(sndptr)
+
+    @property
+    def system_object(self):
+        sysptr = c_int()
+        ckresult(_dll.FMOD_SoundGroup_GetSystemObject(self._ptr, byref(sysptr)))
+        return system.System(sysptr, False)
+
+    @property
+    def volume(self):
+        vol = c_float()
+        ckresult(_dll.FMOD_SoundGroup_GetVolume(self._ptr, byref(vol)))
+        return vol.value
+    @volume.setter
+    def volume(self, vol):
+        ckresult(_dll.FMOD_SoundGroup_SetVolume(self._ptr, c_float(vol)))
+
+    def release(self):
+        ckresult(_dll.FMOD_SoundGroup_Release(self._ptr))
+
+    def stop(self):
+        ckresult(_dll.FMOD_SoundGroup_Stop(self._ptr))
