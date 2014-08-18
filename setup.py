fdbbd4b304dd470e0fa6ff09a45d01093c81c9a9
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2014-08-18 16:35:20 +0200

    Update setup script and add MIT license terms.

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name="pyfmodex", version="0.3.0", author="Lukas Tyrychtr", author_email="lukastyrychtr@gmail.com", url="http://www.github.com/tyrylu/pyfmodex", packages=["pyfmodex"], long_description=open("readme.md", "r").read())
+clfs = []
+clfs.append("Development Status :: 5 - Production/Stable")
+clfs.append("Intended Audience :: Developers")
+clfs.append("License :: OSI Approved :: MIT License")
+clfs.append("Operating System :: Microsoft :: Windows :: Windows XP")
+clfs.append("Operating System :: Microsoft :: Windows :: Windows Vista")
+clfs.append("Operating System :: Microsoft :: Windows :: Windows 7")
+clfs.append("Operating System :: MacOS :: MacOS X")
+clfs.append("Operating System :: POSIX :: Linux")
+clfs.append("Programming Language :: Python :: 2")
+clfs.append("Programming Language :: Python :: 3")
+
+setup(name="pyfmodex", version="0.3.1", author="Lukas Tyrychtr", author_email="lukastyrychtr@gmail.com", url="https://www.github.com/tyrylu/pyfmodex", packages=["pyfmodex"], long_description=open("readme.md", "r").read(), description="Python bindings to the Fmod Ex library.", license="MIT", classifiers=clfs)
