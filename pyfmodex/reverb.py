de5771bd26298d3a6fd3d7a33ff3e04e4c8bf1a4
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2021-04-08 22:12:00 +0200

    Reverb3D setter wants a reference...

--- pyfmodex/reverb.py
+++ pyfmodex/reverb.py
@@ -17,9 +17,10 @@ class Reverb3D(FmodObject):
 
     @_threed_attrs.setter
     def _threed_attrs(self, attrs):
+        pos = VECTOR.from_list(attrs[0])
         self._call_fmod(
             "FMOD_Reverb3D_Set3DAttributes",
-            VECTOR.from_list(attrs[0]),
+            byref(pos),
             c_float(attrs[1]),
             c_float(attrs[2]),
         )
